3b2fd2b65584a5d2a4f15cf6f488d35551853e5a
Thibaut.sabot <thibaut.sabot@adevinta.com>
2022-06-01 16:29:58 +0200

    create branch and commit on save admin post

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import NextAuth from "next-auth";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
       authorization: {
         params: {
           prompt: "consent",
@@ -15,13 +15,14 @@ export default NextAuth({
       },
     }),
   ],
-  jwt: {
-    encryption: true,
-  },
   secret: process.env.SECRET,
   callbacks: {
-    async signIn({ account, profile }) {
-      return profile.email_verified && profile.email === "cesapit@gmail.com";
+    async signIn({ profile }) {
+      return (
+        profile.email_verified as boolean &&
+        (profile.email === process.env.PERSONAL_EMAIL1 ||
+          profile.email === process.env.PERSONAL_EMAIL2)
+      );
     },
   },
 });
