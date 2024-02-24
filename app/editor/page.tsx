8874764994aac0a0fcfc30bbd0401648df476b1e
Thibaut.sabot <thibaut.sabot@adevinta.com>
2024-02-25 00:20:52 +0100

    feat: add tinymce editor and viewer

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,10 +1,61 @@
-export default function Editor() {
+"use client";
+
+import React, { RefObject, useRef } from "react";
+
+import { Editor } from "@tinymce/tinymce-react";
+import { Editor as TinyMCEEditor } from "tinymce";
+
+export default function EditorComponent() {
+  const editorRef = useRef<TinyMCEEditor | null>(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
   return (
-    <div>
-      <h1>Next.js + Tailwind CSS + TypeScript</h1>
-      <p>
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
-    </div>
+    <>
+      <Editor
+        id="tiny-mce-editor"
+        tinymceScriptSrc={"/tinymce/tinymce.min.js"}
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          language: "fr_FR",
+          menubar: false,
+          promotion: false,
+          statusbar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "image",
+            "preview",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "image" +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
   );
 }
